2daf1566934eb7e3f2b02ddaed881f7813003b4a
Niharika-Pal <142835320+Niharika-Pal@users.noreply.github.com>
2023-09-06 06:51:44 +0530

    UpdateProgram.vb

--- ConsoleApp3/Program.vb
+++ ConsoleApp3/Program.vb
@@ -2,39 +2,49 @@ Imports System
 
 Module Program
     Class VEHICLE
-        Private speed As Double
-        Private mile As Double
-        Private type As String
+        Protected speed As Double
+        Protected n As String
+        Protected mile As Double
+        Protected type As String
 
         Public Sub New()
             Dim speed As Double
             speed = 0
         End Sub
 
-        Public Sub New(sped As Double)
+        Public Sub New(name As String, sped As Double)
+            n = name
             speed = sped
+            Console.WriteLine("speed is " & speed)
         End Sub
 
         Public Overridable Sub typ(typ As String)
             type = typ
+            Console.WriteLine("type of vehicle is " & type)
         End Sub
+
         Public Overridable Sub mileage(distance As Double, litres As Double)
             mile = distance / litres
-            Conole.writeline("mileage is " & mile)
+            Console.WriteLine("mileage is " & mile)
         End Sub
+
     End Class
 
     Class Bmw
         Inherits VEHICLE
 
 
-        Public Sub New(sped)
-            MyBase.New()
+        Public Sub New(name, sped)
+            MyBase.New("bmw", 40)
         End Sub
 
-        Public override Sub mileage()
-           MyBase.mileage()
-            Console.WriteLine("mileage is " & mile)
+        Public Overrides Sub mileage(distance As Double, litres As Double)
+            MyBase.mileage(165, 6)
+        End Sub
+         
+
+        Public Overrides Sub typ(typ As String)
+            MyBase.typ("automatic")
         End Sub
     End Class
 
@@ -42,34 +52,28 @@ Module Program
         Inherits VEHICLE
 
 
-        ' Private distance As Double = 250
-        ' Private time As Double = 1
-        ' Private litres As Double = 1
+        Public Sub New(name, sped)
+            MyBase.New("ferrari", 60)
+        End Sub
 
-        ' Public Sub New()
-        '    Dim speed As Double
-        '  speed = distance / time
-        '     Console.WriteLine("speed is " & speed)
-        '  End Sub
+        Public Overrides Sub mileage(distance As Double, litres As Double)
+            MyBase.mileage(178, 6)
+        End Sub
 
-        ' Public Sub mile()
-        ' Dim mileage As Double
-        ''     mileage = distance / litres
-        '   Console.WriteLine("mileage is " & mileage)
-        '  End Sub
-        ''  End Class
 
-        Sub main()
+        Public Overrides Sub typ(typ As String)
+            MyBase.typ("automatic")
+        End Sub
+    End Class
+    
 
-        Dim c1 As Bmw = New Bmw()
 
-        Dim c2 As ferrari = New ferrari()
+    Sub main()
         Console.WriteLine("Information OF BMW")
-        c1.show()
-        c1.mil()
-        Console.WriteLine("Information OF FERRARI")
-        c2.READ()
-        c2.mile()
+        Dim c1 As Bmw = New Bmw("Bmw", 40)
+        c1.typ("automatic")
 
+        c1.mileage(165, 5)
+        Console.ReadLine()
     End Sub
 End Module
